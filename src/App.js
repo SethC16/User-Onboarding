bf41acbf5b57962078c178f979aa71a59bf4ad9a
Seth Cox <Sethcox16@yahoo.com>
2020-01-30 19:16:48 -0500

    Created functiom the ring in required makes a custom inline err msg

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import OnboardForm from './Component/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <OnboardForm />
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
